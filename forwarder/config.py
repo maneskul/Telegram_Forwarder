3d78dd6cf79b387e62aeabd37cf8b087fba3987a
Guilherme Maneskul <guilherme@maneskul.com.br>
2021-06-11 14:34:49 -0300

    Update and rename sample_config.py to config.py

--- forwarder/config.py
+++ forwarder/config.py
@@ -10,7 +10,7 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "617722711:AAHysCHsVcQja6LOlHa88101lKjrxez80-M"  # API key obtained from BotFather
+    API_KEY = "1795086011:AAHMaPml72Ix71l94oqEG9I3E477k3KPcIo"  # API key obtained from BotFather
     OWNER_ID = "582884567"  # If you dont know, run the bot and do /id in your private chat with the bot
 
     # FOR AUTOMATICALLY FORWARDING MESSAGES
